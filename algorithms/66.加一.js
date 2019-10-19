8994a52de2284782aa611de78feb7b96520fb497
Hrealm <952172047@qq.com>
2019-10-19 14:40:58 +0800

    feat:66.plus-one.js

--- algorithms/66.加一.js
+++ algorithms/66.加一.js
@@ -53,7 +53,7 @@ var plusOne = function(digits) {
                 digits[i] = 0;
                 i--;
                 if(i < 0){
-                    digits.splice(0,0,1);
+                    digits.unshift(1);
                     return digits;
                 }
             }
