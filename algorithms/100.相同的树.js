07c66f1fc38be1e6651d2e7027a7149d651ba680
Hrealm <952172047@qq.com>
2019-10-27 02:13:46 +0800

    feat:100.same-tree.js

--- algorithms/100.相同的树.js
+++ algorithms/100.相同的树.js
@@ -65,6 +65,15 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
+
+    if(!p && !q) return true;
+    if(!p || !q) return false;
+    if(p.val == q.val){
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }else{
+        return false;
+    }
+
     
 };
 // @lc code=end
