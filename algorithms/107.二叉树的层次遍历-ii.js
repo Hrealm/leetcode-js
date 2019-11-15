5ddc0b93d5cf83e39c5b0093cf72c23450ad4d0e
Hrealm <952172047@qq.com>
2019-11-15 23:59:52 +0800

    feat:107.binary-tree-level-order-traversal-ii.js

--- algorithms/107.二叉树的层次遍历-ii.js
+++ algorithms/107.二叉树的层次遍历-ii.js
@@ -49,7 +49,23 @@
  * @return {number[][]}
  */
 var levelOrderBottom = function(root) {
+    if (!root) {
+        return []
+      }
+      const extractNodes = nodes => {
+        if (nodes.length === 0) {
+          return []
+        }
+        let ret = extractNodes(nodes.reduce((r, n) => {
+          if (n.left) r.push(n.left)
+          if (n.right) r.push(n.right)
+          return r
+        }, []))
+        ret.push(nodes.map(n => n.val))
     
+        return ret
+      }
+      return root ? extractNodes([root]) : []
 };
 // @lc code=end
 
