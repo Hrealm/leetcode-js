013994cce0018e965017a7b29ca4c3c8a106d943
Hrealm <952172047@qq.com>
2021-07-06 23:58:07 +0800

    feat: 136.single-number

---
+++ algorithms/136.只出现一次的数字.js
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=javascript
+ *
+ * [136] 只出现一次的数字
+ *
+ * https://leetcode-cn.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (71.64%)
+ * Likes:    1912
+ * Dislikes: 0
+ * Total Accepted:    432.1K
+ * Total Submissions: 603.2K
+ * Testcase Example:  '[2,2,1]'
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 
+ * 说明：
+ * 
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * 
+ * 示例 1:
+ * 
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i+1; j < nums.length; j++) {
+      if (nums[i] == nums[j]) {
+        nums.splice(i,1)
+        nums.splice(j-1,1)
+        singleNumber(nums)
+      }
+    }
+  }
+  return nums[0]
+};
+// @lc code=end
+
